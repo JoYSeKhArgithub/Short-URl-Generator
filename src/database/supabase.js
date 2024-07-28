a39b038888abe438c5063de0b81d9c22226a49f8
Joysekhar Banerjee <bjoysekhar@gmail.com>
2024-07-28 22:34:04 +0530

    Update Subpabase.js

--- src/database/supabase.js
+++ src/database/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_KEY;
+export const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-
+console.log(supabase);
 export default supabase;
